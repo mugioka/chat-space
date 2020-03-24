36ee7d212a2cde76ffa1d012594f617c7dbb052b
MugiOka <okamugi0722@gmail.com>
2020-03-24 22:48:14 +0900

    create_ajax

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,54 @@
+$(function(){
+  function buildHTML(message){
+    if (message.image) {
+      var html = `<div class="user-content">
+                    <h3 class="user-name">
+                      ${message.user_name}
+                    </h3>
+                    <p class="user-time">
+                      ${message.created_at}
+                    </p>
+                  </div>
+                  <div class="text-message">
+                    ${message.content}
+                    ${message.image}
+                  </div>`
+      return html;
+    } else {
+      var html = `<div class="user-content">
+                    <h3 class="user-name">
+                      ${message.user_name}
+                    </h3>
+                    <p class="user-time">
+                      ${message.created_at}
+                    </p>
+                 </div>
+                 <div class="text-message">
+                   ${message.content}
+                 </div>`
+      return html
+    }
+  }
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.main-contents').append(html);
+      $('.main-contents').animate({ scrollTop: $('.main-contents')[0].scrollHeight});
+      $('form')[0].reset();
+    })
+    .fail(function(){
+      alert("メッセージ送信error");
+    })
+  });
+});
